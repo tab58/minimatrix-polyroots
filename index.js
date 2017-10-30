6fdc6cc6889e7dec61e988abda72d554d0ab731c
Tim Bright <capoeirista.muralha@gmail.com>
2017-10-30 16:54:22 -0700

    Added linear root, refactored for better function references

--- index.js
+++ index.js
@@ -116,124 +116,138 @@ function qdrtc (A, B, C) {
   ];
 }
 
-const Roots = {
-  getQuadraticRoots: function (A, B, C) {
-    // method based on Kahan's notes "To Solve a Real Cubic Equation"
-    return qdrtc(A, B, C);
-  },
-  getCubicRoots: function (A, B, C, D) {
-    // method based on Kahan's notes "To Solve a Real Cubic Equation"
-    let X;
-    let a;
-    let b1;
-    let c2;
-    if (A === 0) {
-      X = undefined;
-      a = B;
-      b1 = C;
-      c2 = D;
-    } else if (D === 0) {
-      X = 0;
-      a = A;
-      b1 = B;
-      c2 = C;
-    } else {
-      a = A;
-      X = -(B / A) / 3;
-      let evalInfo = evaluate(X, A, B, C, D);
-      let q = evalInfo.Q;
-      let dq = evalInfo.dQ;
-      b1 = evalInfo.B1;
-      c2 = evalInfo.C2;
-
-      let t = q / A;
-      let r = _Math.pow(_Math.abs(t), 1 / 3);
-      const s = _Math.sign(t);
-      t = -dq / A;
-      if (t > 0) {
-        r = 1.324717957244746 * _Math.max(r, _Math.sqrt(t));
-      }
-      let x0 = X - s * r;
-      if (x0 !== X) {
-        const den = 1 + (100 * EPS);
-        do {
-          X = x0;
-          evalInfo = evaluate(X, A, B, C, D);
-          q = evalInfo.Q;
-          dq = evalInfo.dQ;
-          b1 = evalInfo.B1;
-          c2 = evalInfo.C2;
-          x0 = (dq === 0 ? X : X - (q / dq) / den);
-        } while (s * x0 > s * X);
-        if (_Math.abs(A) * X * X > _Math.abs(D / X)) {
-          c2 = -D / X;
-          b1 = (c2 - C) / X;
-        }
-      }
-    }
-    const roots = [];
-    if (X !== undefined) {
-      roots.push(makeRoot(X, 0));
-    }
-    const quadInfo = qdrtc(a, b1, c2);
-    return roots.concat(quadInfo);
-  },
-  getQuarticRoots: function (a, b, c, d, e) {
-    // See link for method:
-    // https://math.stackexchange.com/questions/785/is-there-a-general-formula-for-solving-4th-degree-equations-quartic
-    if (a === 0) {
-      return Roots.getCubicRoots(b, c, d, e);
-    }
-    // compute the "depressed" quartic via the substitution x = z - (b / 4a):
-    //    az^4 + Bz^2 + Cz + D = 0
-    //    B, C, D are reals
-    const B = c - (3 * b * b) / (8 * a);
-    const C = d - (b * c) / (2 * a) + (b * b * b) / (8 * a * a);
-    const D = e - (b * d) / (4 * a) + (b * b * c) / (16 * a * a) - (3 * b * b * b * b) / (256 * a * a * a);
-    // compute the "depressed" monic quartic:
-    //    z^4 + pz^2 + qz + r = 0
-    //    p, q, r are reals
-    const p = B / a;
-    const q = C / a;
-    const r = D / a;
-    // Since p, q, r, are reals, Descartes Factorization:
-    //    (z^2 + mz + n)(z^2 + sz + t) = 0
-    //     m, n, s, t are reals
-    // Solving for constants yields:
-    //    (z^2 + mz + n)(z^2 - mz + (r/n)) = 0
-    // We also get:
-    //    m^6 + 2pm^4 + (p^2 - 4r)m^2 - q^2 = 0
-    // Substitute w = m^2:
-    //    w^3 + 2pw^2 + (p^2 - 4r)w - q^2 = 0
-    // If m is real, then w must be real and w >= 0.
-    // For t to be real, then w > 0.
-    const ws = Roots.getCubicRoots(1, 2 * p, p * p - 4 * r, -q * q)
-      .filter(root => root.imag === 0)
-      .map(root => root.real)
-      .filter(w => w > 0);
-
-    const zCoeffs = [];
-    ws.forEach(w => {
-      const m0 = Math.sqrt(w);
-      const m1 = -m0;
-      const n = 0.5 * (p + w);
-      const n0 = n - q / (2 * m0);
-      const n1 = n - q / (2 * m1);
-      zCoeffs.push({ m: m0, n: n0 });
-      zCoeffs.push({ m: m1, n: n1 });
-    });
-    const zs = [];
-    zCoeffs.forEach(zCoeff => {
-      const { m, n } = zCoeff;
-      const quadInfo1 = Roots.getQuadraticRoots(1, m, n);
-      zs.push.apply(zs, quadInfo1);
-      const quadInfo2 = Roots.getQuadraticRoots(1, -m, r / n);
-      zs.push.apply(zs, quadInfo2);
-    });
-    const uniqueZ = getDistinctRoots(zs);
-    uniqueZ.forEach(z => { z.real += -b / (4 * a); });
-    return uniqueZ;
+const getLinearRoot = function (A, B) {
+  // P(x) = A*x + B
+  if (A !== 0) {
+    return [makeRoot(-B / A, 0)];
+  } else {
+    return [];
   }
 };
 
-module.exports = Roots;
+const getQuadraticRoots = function (A, B, C) {
+  // method based on Kahan's notes "To Solve a Real Cubic Equation"
+  return qdrtc(A, B, C);
+};
+
+const getCubicRoots = function (A, B, C, D) {
+  // method based on Kahan's notes "To Solve a Real Cubic Equation"
+  let X;
+  let a;
+  let b1;
+  let c2;
+  if (A === 0) {
+    X = undefined;
+    a = B;
+    b1 = C;
+    c2 = D;
+  } else if (D === 0) {
+    X = 0;
+    a = A;
+    b1 = B;
+    c2 = C;
+  } else {
+    a = A;
+    X = -(B / A) / 3;
+    let evalInfo = evaluate(X, A, B, C, D);
+    let q = evalInfo.Q;
+    let dq = evalInfo.dQ;
+    b1 = evalInfo.B1;
+    c2 = evalInfo.C2;
+
+    let t = q / A;
+    let r = _Math.pow(_Math.abs(t), 1 / 3);
+    const s = _Math.sign(t);
+    t = -dq / A;
+    if (t > 0) {
+      r = 1.324717957244746 * _Math.max(r, _Math.sqrt(t));
+    }
+    let x0 = X - s * r;
+    if (x0 !== X) {
+      const den = 1 + (100 * EPS);
+      do {
+        X = x0;
+        evalInfo = evaluate(X, A, B, C, D);
+        q = evalInfo.Q;
+        dq = evalInfo.dQ;
+        b1 = evalInfo.B1;
+        c2 = evalInfo.C2;
+        x0 = (dq === 0 ? X : X - (q / dq) / den);
+      } while (s * x0 > s * X);
+      if (_Math.abs(A) * X * X > _Math.abs(D / X)) {
+        c2 = -D / X;
+        b1 = (c2 - C) / X;
+      }
+    }
+  }
+  const roots = [];
+  if (X !== undefined) {
+    roots.push(makeRoot(X, 0));
+  }
+  const quadInfo = qdrtc(a, b1, c2);
+  return roots.concat(quadInfo);
+};
+
+const getQuarticRoots = function (a, b, c, d, e) {
+  // See link for method:
+  // https://math.stackexchange.com/questions/785/is-there-a-general-formula-for-solving-4th-degree-equations-quartic
+  if (a === 0) {
+    return getCubicRoots(b, c, d, e);
+  }
+  // compute the "depressed" quartic via the substitution x = z - (b / 4a):
+  //    az^4 + Bz^2 + Cz + D = 0
+  //    B, C, D are reals
+  const B = c - (3 * b * b) / (8 * a);
+  const C = d - (b * c) / (2 * a) + (b * b * b) / (8 * a * a);
+  const D = e - (b * d) / (4 * a) + (b * b * c) / (16 * a * a) - (3 * b * b * b * b) / (256 * a * a * a);
+  // compute the "depressed" monic quartic:
+  //    z^4 + pz^2 + qz + r = 0
+  //    p, q, r are reals
+  const p = B / a;
+  const q = C / a;
+  const r = D / a;
+  // Since p, q, r, are reals, Descartes Factorization:
+  //    (z^2 + mz + n)(z^2 + sz + t) = 0
+  //     m, n, s, t are reals
+  // Solving for constants yields:
+  //    (z^2 + mz + n)(z^2 - mz + (r/n)) = 0
+  // We also get:
+  //    m^6 + 2pm^4 + (p^2 - 4r)m^2 - q^2 = 0
+  // Substitute w = m^2:
+  //    w^3 + 2pw^2 + (p^2 - 4r)w - q^2 = 0
+  // If m is real, then w must be real and w >= 0.
+  // For t to be real, then w > 0.
+  const ws = getCubicRoots(1, 2 * p, p * p - 4 * r, -q * q)
+    .filter(root => root.imag === 0)
+    .map(root => root.real)
+    .filter(w => w > 0);
+
+  const zCoeffs = [];
+  ws.forEach(w => {
+    const m0 = Math.sqrt(w);
+    const m1 = -m0;
+    const n = 0.5 * (p + w);
+    const n0 = n - q / (2 * m0);
+    const n1 = n - q / (2 * m1);
+    zCoeffs.push({ m: m0, n: n0 });
+    zCoeffs.push({ m: m1, n: n1 });
+  });
+  const zs = [];
+  zCoeffs.forEach(zCoeff => {
+    const { m, n } = zCoeff;
+    const quadInfo1 = getQuadraticRoots(1, m, n);
+    zs.push.apply(zs, quadInfo1);
+    const quadInfo2 = getQuadraticRoots(1, -m, r / n);
+    zs.push.apply(zs, quadInfo2);
+  });
+  const uniqueZ = getDistinctRoots(zs);
+  uniqueZ.forEach(z => { z.real += -b / (4 * a); });
+  return uniqueZ;
+};
+
+module.exports = {
+  getLinearRoot,
+  getQuadraticRoots,
+  getCubicRoots,
+  getQuarticRoots
+};
